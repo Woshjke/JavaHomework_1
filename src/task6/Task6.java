fccdeebae31b2aceaad0de3a9b86d83942de6461
Woshjke <z.vashkevich1999@gmail.com>
2019-02-19 14:44:18 +0300

    Completed task6

--- src/task6/Task6.java
+++ src/task6/Task6.java
@@ -1,7 +1,17 @@
 package task6;
 
-public class Task6 {
-    public static void main(String[] args){
+import java.util.Scanner;
 
+public class Task6
+{
+    public static void main(String[] args)
+    {
+        String ticketNumber;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Input ticket number: ");
+        ticketNumber = scanner.next();
+        if (LuckyTicket.isLuckyTicket(ticketNumber)) System.out.println("This is lucky ticket!");
+        else System.out.println("This is ragular ticket!");
     }
+
 }
