d13292b5cae535db5b6fdce8b4e68bba79d97595
zvash <z.vashkevich1999@gmail.com>
2019-02-20 17:16:03 +0300

    Task2 input fix

--- src/Lab1/task1/Task1.java
+++ src/Lab1/task1/Task1.java
@@ -3,6 +3,7 @@ package Lab1.task1;
 import java.util.Scanner;
 
 public class Task1 {
+
     public static void main(String[] args) {
         double temperatureInCelsius;
         Scanner scanner = new Scanner(System.in);
--- src/Lab1/task2/Task2.java
+++ src/Lab1/task2/Task2.java
@@ -3,18 +3,31 @@ package Lab1.task2;
 import java.util.Scanner;
 
 public class Task2 {
+
     public static void main(String[] args) {
         double x1, x2, y1, y2, z1, z2;
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите координаты первой точки: ");
+        while (!scanner.hasNextDouble()) {
+            System.out.println("Это не число! Попытайтесь снова: ");
+            scanner.next();
+        }
         x1 = scanner.nextDouble();
         x2 = scanner.nextDouble();
         scanner.nextLine();
         System.out.println("Введите координаты второй точки: ");
+        while (!scanner.hasNextDouble()) {
+            System.out.println("Это не число! Попытайтесь снова: ");
+            scanner.next();
+        }
         y1 = scanner.nextDouble();
         y2 = scanner.nextDouble();
         scanner.nextLine();
         System.out.println("Введите координаты третьей точки: ");
+        while (!scanner.hasNextDouble()) {
+            System.out.println("Это не число! Попытайтесь снова: ");
+            scanner.next();
+        }
         z1 = scanner.nextDouble();
         z2 = scanner.nextDouble();
         scanner.nextLine();
