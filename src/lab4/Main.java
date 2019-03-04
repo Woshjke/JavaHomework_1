a55e4342048c301ff1e9d7ea55e3581cfa477eb2
Evgeniy Vashkevich <z.vashkevich1999@gmail.com>
2019-03-04 17:27:36 +0300

    lab 4 last tasks finished + small refactor

--- src/lab4/Main.java
+++ src/lab4/Main.java
@@ -1,10 +1,9 @@
 package lab4;
 
-import lab4.task10.Task10;
-import lab4.task9.Task9;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println(Task9.isPhoneNumber("+375291212122"));
+
     }
 }
--- src/lab4/task5/Task5.java
+++ src/lab4/task5/Task5.java
@@ -1,5 +1,8 @@
 package lab4.task5;
 
+import java.util.Map;
+import java.util.TreeMap;
+
 public class Task5 {
     /**
      * Строка состоит из слов. Слова – это цифры.
@@ -7,7 +10,32 @@ public class Task5 {
      *
      * @param str - строка
      */
-    public static void task5(String str) {
+    public static String customSort(String str) {
+        String[] arrOfNums = str.split("\\D+");
+        Integer[] arrOfSums = new Integer[arrOfNums.length];
+        StringBuilder outputString = new StringBuilder();
 
+        //Разбиваю каждое число на цифры и суммирую их
+        for (int i = 0; i < arrOfNums.length; i++) {
+            int temp = 0;
+            String[] tempArray = arrOfNums[i].split("");
+            for (String iter : tempArray) {
+                temp += Integer.parseInt(iter);
+            }
+            arrOfSums[i] = temp;
+        }
+
+        //Создаем дерево, где ключ - это сумма цифер числа, а значение - само число.
+        Map<Integer, String> map = new TreeMap<>();
+        for (int i = 0; i < arrOfNums.length; i++) {
+            map.put(arrOfSums[i], arrOfNums[i]);
+        }
+
+        //Так как в дереве все элементы отсортированны, то просто формируем новую строку, проходясь по значениям дерева.
+        for (Map.Entry<Integer, String> entry : map.entrySet()) {
+            outputString.append(entry.getValue() + " ");
+        }
+
+        return outputString.toString();
     }
 }
