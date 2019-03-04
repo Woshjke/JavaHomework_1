c9b106c55e204c798487eb6f05c8aeca4f7a6bc8
Evgeniy Vashkevich <z.vashkevich1999@gmail.com>
2019-03-04 15:35:35 +0300

    lab 4 refactor

--- src/lab4/task10/Task10.java
+++ src/lab4/task10/Task10.java
@@ -1,4 +1,14 @@
 package lab4.task10;
 
 public class Task10 {
+    /**
+     * Проверить комметарий пользователя на наличие html тегов и убрать их при необходимости
+     *
+     * @param string - проверяемая строка
+     * @return - строка с удаленными html-тегами
+     */
+    public static String removeHtmlTags(String string) {
+        String outputString = string.replaceAll("\\<.*?>", "");
+        return outputString;
+    }
 }
--- src/lab4/task9/Task9.java
+++ src/lab4/task9/Task9.java
@@ -1,7 +1,15 @@
 package lab4.task9;
 
 public class Task9 {
+    /**
+     * Является ли строка валидным мобильным номером телефона РБ
+     * соит обратить внимание, что номер может начинаться с символа «+» и может быть без него,
+     * «8» и нет, и другие варианты – проверка должна осуществляться регулярными выражениями.
+     *
+     * @param phoneNumber - номер телефона
+     * @return - является ли номером телефона РБ
+     */
     public static boolean isPhoneNumber(String phoneNumber) {
-        return phoneNumber.matches("^([+][375]).{11,13}$") || phoneNumber.matches("^[80].{10,}$");
+        return phoneNumber.matches("^([+][375]).{11}$") || phoneNumber.matches("^[80].{10}$");
     }
 }
