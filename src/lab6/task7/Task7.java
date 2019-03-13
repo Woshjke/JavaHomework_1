7a0f76c5e2e6f5e6037f46cd1837950ef0e9851f
Evgeniy Vashkevich <z.vashkevich1999@gmail.com>
2019-03-13 13:15:09 +0300

    lab6 task7

--- src/lab6/task7/Task7.java
+++ src/lab6/task7/Task7.java
@@ -5,6 +5,11 @@ import java.net.URISyntaxException;
 import java.util.Random;
 
 public class Task7 {
+    /**
+     * У нас есть метод. Он выбрасывает два исключения, унаследованные от Exception, и два унаследованных от RuntimeException:
+     * NullPointerException, ArithmeticException, FileNotFoundException, URISyntaxException.
+     * Задание: перехват NullPointerException и FileNotFoundException, без перехвата  и URISyntaxException. Как это сделать?
+     */
     public static void execute() throws NullPointerException, ArithmeticException, FileNotFoundException, URISyntaxException {
         Random random = new Random();
         int num = random.nextInt(5);
